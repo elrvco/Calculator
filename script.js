842151027e52f9790581812c3e5a8be356fa601a
elruco <ing.garcia1995@gmail.com>
2024-05-03 23:33:36 -0600

    Enabled all buttons and working in a new design
    Added functions to all buttons.
    Working in a new HTML structure.Added a parent
    container for buttons.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let firstNum ;
 let operator;
 let secondNum;
-let memo = [];
+let memoOperator = [];
 
-const add = (x,y) => x + y;
-const subtract = (x,y) => x - y;
-const multiply = (x,y) => x * y;
-const divide = (x,y) => x / y;
+const add = (x,y) => Number(x) + Number(y);
+const subtract = (x,y) => Number(x) - Number(y);
+const multiply = (x,y) => Number(x) * Number(y);
+const divide = (x,y) => Number(x) / Number(y);
+const percent = (x,y) => (Number(y)/100)*Number(x);
 
 
 function operate(operator,firstNum,secondNum){
@@ -18,17 +19,98 @@ function operate(operator,firstNum,secondNum){
         return multiply(firstNum,secondNum);
     }else if (operator == '/'){
         return divide(firstNum,secondNum);
+    }else if (operator == '%'){
+        return percent(firstNum,secondNum);
     }
+};
 
-}
-
+let counter=0;
 let display=document.querySelector('#display');
+display.textContent = '0';
 const operandBtns = document.querySelectorAll('.operand');
-operandBtns.forEach((number)=>{
+operandBtns.forEach((number)=>{ //this method displays the numbers on the calculator display
     number.addEventListener('click',()=>{
-        display.textContent = number.textContent;
-        memo.unshift(display.textContent);
-    })
-})
 
-const operatorBtns = document.querySelectorAll('.operator')
+        if ((counter>0)&&(!(secondNum === undefined))){
+            display.textContent += number.textContent;
+            secondNum = display.textContent;
+        }
+        if ((counter>0)&&(secondNum === undefined)&&(!(display.textContent == '0.'))){
+            display.textContent = number.textContent;
+            secondNum = number.textContent;
+        }
+        if ((counter === 0) && (!(display.textContent === '0'))){
+            display.textContent += number.textContent;
+            firstNum = display.textContent;
+        }
+        if ((display.textContent === '0')&&(counter === 0)){
+            display.textContent = number.textContent;
+            firstNum = display.textContent;
+        }
+    })
+});
+
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach((operator)=>{
+    operator.addEventListener('click',()=>{
+        if(!(firstNum===undefined)){
+            counter++;
+            memoOperator[counter-1] = operator.textContent;
+                if ((counter>=2)&&!(secondNum===undefined)){
+                    let result= operate(memoOperator[counter-2],firstNum,secondNum);
+                    firstNum= result;
+                    display.textContent = firstNum;
+                    secondNum = undefined;
+                }
+        }
+    })
+});
+
+const equal = document.querySelector('#equal');
+equal.addEventListener('click',()=>{
+    if ((counter>0)&&(!(secondNum === undefined))){
+        let lastOperator = memoOperator[memoOperator.length - 1];
+        let equalResult = operate(lastOperator,firstNum,secondNum);
+        firstNum = equalResult;
+        display.textContent = firstNum;
+        secondNum = undefined;
+    }
+});
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click',()=>{
+    firstNum = undefined;
+    secondNum = undefined;
+    memoOperator = [];
+    counter = 0;
+    display.textContent = '0';
+});
+
+const signBtn = document.querySelector('#sign');
+signBtn.addEventListener('click',()=>{
+    if (!(firstNum === undefined)&&(secondNum === undefined)){
+       display.textContent = Number(display.textContent) * -1;
+       firstNum = firstNum * -1;
+    }
+    if (!(firstNum === undefined)&&!(secondNum === undefined)){
+        display.textContent = Number(display.textContent) * -1;
+        secondNum = secondNum * -1;
+     }
+});
+
+const decimalBtn = document.querySelector('#decimal');
+decimalBtn.addEventListener('click',()=>{
+    if (display.textContent == 0){
+        display.textContent += decimalBtn.textContent;
+    }
+    if ((counter === 0)&&(Number.isInteger(+firstNum))){
+        display.textContent += decimalBtn.textContent;
+    }
+    if ((counter>0)&&(Number.isInteger(+secondNum))){
+        display.textContent += decimalBtn.textContent;
+    }
+    if ((counter>0)&&(secondNum === undefined)){
+        display.textContent = '0'+decimalBtn.textContent;
+        secondNum = display.textContent
+    }
+})
